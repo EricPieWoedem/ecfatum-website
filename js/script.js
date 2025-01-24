bcb0c4836c213a50210ce75f0505f6901e253d30
EricPieWoedem <coco.pie3030@gmail.com>
2025-01-24 14:45:06 +0000

    more functionalities and styling added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,61 @@
+// Hamburger sign
+// Select the hamburger button and the navigation menu
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('nav ul');
+const link =document.querySelectorAll('li')
+
+// Toggle the "active" class on the hamburger and nav menu when clicked
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active'); // Animate the hamburger icon
+  navMenu.classList.toggle('active');
+  const active = document.querySelector('active'); // Show or hide the menu
+});
+
+// smooth scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('in-view');
+    }
+  });
+});
+
+document.querySelectorAll('[data-animate]').forEach(el => {
+  observer.observe(el);
+});
+
+// Back to top button
+const backToTop = document.getElementById('backToTop');
+
+// Show/hide the "Back to Top" button with animation
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTop.classList.add('visible'); // Add the class for smooth showing
+  } else {
+    backToTop.classList.remove('visible'); // Remove the class to hide
+  }
+});
+
+// Scroll back to the top when the button is clicked
+backToTop.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
+
+
+
+// carousel
 document.addEventListener("DOMContentLoaded", function () {
   const prevBtn = document.querySelector(".carousel-btn.prev");
   const nextBtn = document.querySelector(".carousel-btn.next");
